260b22ad961c3caf66b20e4b428ad737021a9eab
Tommy Danielsson <tdserviceco@gmail.com>
2021-03-15 00:39:57 +0100

    trying again without the api server

--- server-assets/utils/utils.js
+++
@@ -1,6 +0,0 @@
-function generateOrderNr() {
-    let letters = ['X', 'Y', 'Z'];
-    return `AB${Date.now()}${letters[Math.floor(Math.random() * letters.length)]}`;
-}
-
-module.exports = { generateOrderNr }
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const beansRouter = require('./server-assets/routes/beans');
 const path = require('path');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,7 +7,6 @@ app.use(cors())
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/api/beans', beansRouter)
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
